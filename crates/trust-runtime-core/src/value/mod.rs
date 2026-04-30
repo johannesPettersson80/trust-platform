ecbb2a6bb14c934e2c9a78b160dab67aaf8129a8
johannes <johannes_salomon@hotmail.com>
2026-05-01 01:30:49 +0200

    Move runtime sizing into core

--- crates/trust-runtime-core/src/value/mod.rs
+++ crates/trust-runtime-core/src/value/mod.rs
@@ -5,11 +5,15 @@
 pub mod datetime;
 pub mod partial_access;
 mod reference;
+#[cfg(feature = "hir")]
+pub mod size;
 pub mod string_semantics;
 pub mod types;
 
 pub use datetime::*;
 pub use partial_access::*;
 pub use reference::*;
+#[cfg(feature = "hir")]
+pub use size::*;
 pub use string_semantics::*;
 pub use types::*;
--- crates/trust-runtime/src/value/mod.rs
+++ crates/trust-runtime/src/value/mod.rs
@@ -4,12 +4,11 @@
 
 mod defaults;
 mod reference;
-mod size;
 
 pub use defaults::*;
 pub use reference::*;
-pub use size::*;
 pub use trust_runtime_core::value::datetime::*;
 pub use trust_runtime_core::value::partial_access::*;
+pub use trust_runtime_core::value::size::*;
 pub(crate) use trust_runtime_core::value::string_semantics::*;
 pub use trust_runtime_core::value::types::*;
