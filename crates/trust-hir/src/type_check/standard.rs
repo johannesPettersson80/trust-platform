d3168a9a8be6b549555cde4eac71ee6428470701
teunreyniers <teun.reyniers@hotmail.be>
2026-06-06 16:25:49 +0200

    Add HIR type-checking for IS_VALID & IS_VALID_BCD
    The IS_VALID and IS_VALID_BCD where already defined in the
    stdlib/validate.rs but where not accessible by the compiler
    because they where not defined in the hir parser.
    
    This commits add there definitions to the HIR layer StandardChecker
    validating operand types.

--- crates/trust-hir/src/type_check/standard.rs
+++ crates/trust-hir/src/type_check/standard.rs
@@ -10,6 +10,7 @@ mod numeric;
 mod selection;
 mod string;
 mod time;
+mod validate;
 
 pub(in crate::type_check) use helpers::is_execution_param;
 
@@ -79,6 +80,8 @@ impl<'a, 'b> StandardChecker<'a, 'b> {
                     self.infer_split_date_time_call(node, &upper)
                 }
                 "DAY_OF_WEEK" => self.infer_day_of_week_call(node),
+                "IS_VALID" => self.infer_is_valid_call(node),
+                "IS_VALID_BCD" => self.infer_is_valid_bcd_call(node),
                 _ => return None,
             };
 
